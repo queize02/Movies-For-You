cd556199008f17afbd048ada1fa5549e58a0cc0f
queize02 <queize0208@gmail.com>
2026-05-13 13:24:36 +0200

    tenoste admin

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def movie_detail(movie_id):
 # --- ROUTE ADMINISTRATION ---
 @app.route('/admin_ajouter', methods=['GET', 'POST'])
 def admin_ajouter():
-    if 'user' not in session or session['user'] != "wQueize_":
+    if 'user' not in session or session['user'] != "wQueize_ " and session['user'] != "tenoste":
         return "Accès interdit", 403
     
     if request.method == 'POST':
